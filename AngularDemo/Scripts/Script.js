fe60d34639505aaa8b0f6b4cd3c87fbb44cc3616
ashenbrownfox <newhoopsdynasty@gmail.com>
2016-05-26 22:32:27 -0400

    Playing with modules and Controllers

---
+++ AngularDemo/Scripts/Script.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="angular.min.js" />
+
+//create the Module
+var myApp = angular.module("myModule", []);
+
+//create the controller
+var myController = function($scope) {
+$scope.message = "AngularJS Tutorial";
+}
+//register controller with odel
+myApp.controller("myController", myController);
