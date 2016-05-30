5108b43aacbadbe3ee439280a37d78b7e2b4e282
ashenbrownfox <newhoopsdynasty@gmail.com>
2016-05-29 23:08:34 -0400

    Sorting Test

--- AngularDemo/Scripts/Script.js
+++ AngularDemo/Scripts/Script.js
@@ -17,10 +17,12 @@ myApp.controller("myController", function ($scope) {
     };
     var employees = [
         { firstName: "Kobe", lastName: "Bryant", gender: "Male", salary: 55000, likes: 0, dislikes: 0 },
-        { firstName: "LeBron", lastName: "James", gender: "Male", salary: 55000, likes:0, dislikes: 0 },
+        { firstName: "LeBron", lastName: "James", gender: "Male", salary: 85000, likes:0, dislikes: 0 },
         { firstName: "Kevin", lastName: "Durant", gender: "Male", salary: 75000, likes: 0, dislikes: 0 },
         { firstName: "Russell", lastName: "Westbrook", gender: "Male", salary: 60000, likes: 0, dislikes: 0 },
-        { firstName: "Step", lastName: "Curry", gender: "Female", salary: 65000, likes: 0, dislikes: 0 }
+        { firstName: "Steph", lastName: "Curry", gender: "Female", salary: 65000, likes: 0, dislikes: 0 },
+        { firstName: "Klay", lastName: "Thompson", gender: "Female", salary: 56000, likes: 0, dislikes: 0 },
+        { firstName: "Draymond", lastName: "Green", gender: "Male", salary: 49000, likes: 0, dislikes: 0 }
     ];
     $scope.employees = employees;
     $scope.incrementLikes = function(employees) {
@@ -31,6 +33,8 @@ myApp.controller("myController", function ($scope) {
     };
     $scope.player = player;
     $scope.country = country;
+    $scope.rowLimit = 7;
+    $scope.sortColumn = "name";
     $scope.message = "Two way data binding!";
     $scope.greeting = "Hello Angular!"
 });
